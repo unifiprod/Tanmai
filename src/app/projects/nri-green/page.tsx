8ef1d3ef093b39e0245d3a9c44666cc693c84805
Dev RealEase <Dev@realeasetech.in>
2025-01-19 23:50:37 +0530

    fixed nri green page

---
+++ src/app/projects/nri-green/page.tsx
@@ -0,0 +1,7 @@
+export default function page() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <p className="sm:text-9xl text-xl">Page under Development</p>
+    </div>
+  );
+}
